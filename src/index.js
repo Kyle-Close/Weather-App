ffd53eeed94bc0ee4c3013e57d9d9db3a30cb808
Kyle <k.james.close@gmail.com>
2023-03-09 23:24:59 -0800

    added some more...

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ export let selectors = new DOMSelectors();
 
 // Default location when the page loads.
 displayWeatherData("Waterdown");
+
+// Things to add to page
+/* 
+  - timezone (use this to get the dates... Im not sure this is the best way. The API  may have dates)
+  
+
+  **** If I wanna get crazy ****
+  - wind -> deg (have an arrow that points in the wind direction based on deg on a compass)
+
+
+*/
